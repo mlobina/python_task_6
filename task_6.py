f5cfc11616d42829a473e3ce1b34630294821a50
Marina Lobina <m.v.lobina@gmail.com>
2020-11-16 02:21:13 +0300

    add classes

---
+++ task_6.py
@@ -0,0 +1,72 @@
+class Geese:
+    pass
+
+    def __init__(self, name, weight, sound='ha-ha-ha'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+
+goose1 = Geese('Grey', 3)
+goose2 = Geese('White', 4)
+
+
+class Cows:
+    pass
+
+    def __init__(self, name, weight, sound='mu-mu-mu'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+
+cow1 = Cows('Manka', 800)
+
+
+class Sheep:
+    pass
+
+    def __init__(self, name, weight, sound='be-be-be'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+
+sheep1 = Sheep('Barashek', 100)
+sheep2 = Sheep('Curle', 120)
+
+class Hens:
+    pass
+
+    def __init__(self, name, weight, sound='ku-ka-re-ku'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+
+hen1 = Hens('Ko-Ko', 2)
+hen2 = Hens('Kukareku', 3)
+
+class Goats:
+    pass
+
+    def __init__(self, name, weight, sound='me-me-me'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+
+goat1 = Goats('Horns', 200)
+goat2 = Goats('Hoofs', 240)
+
+
+class Ducks:
+    pass
+
+    def __init__(self, name, weight, sound='kry-kry-kry'):
+        self.name = name
+        self.weight = weight
+        self.sound = sound
+
+duck1 = Ducks('Krykva', 3)
+
