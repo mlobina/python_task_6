ea8c7dbe734c1d37d05c956691268e0b6de0c0fa
Marina Lobina <mvlobina@gmsil.com>
2020-11-19 09:43:25 +0300

    Task_6.2 adwanced

--- task_6.2.py
+++ task_6.2.py
@@ -1,9 +1,8 @@
 class Track:
+    my_track_list = []
 
-    track_list = []
-
-    def __init__(self, name, album, duration):
-        Track.track_list.append(self)
+    def __init__(self, name, album, duration):  # атрибут album добавлен для автоматизации добавления треков в альбом
+        Track.my_track_list.append(self)
         self.name = name
         self.album = album
         self.duration = duration
@@ -13,22 +12,26 @@ class Track:
 
 
 class Album:
+    my_album_list = []
 
-    album_list = []
-
-    def __init__(self, name, group, track_list=[]):
-        Album.album_list.append(self)
+    def __init__(self, name, group):
+        Album.my_album_list.append(self)
         self.name = name
         self.group = group
-        self.track_list = track_list
+        self.track_list = []
 
     def present(self):
         print(f'Let me present a new album "{self.name}" of the cool group "{self.group}"')
 
+    def add_track(self, track_name):
+        self.track_name = track_name
+        for x in Track.my_track_list:
+            if track_name == x.name:
+                self.track_list.append(x)
 
-    def add_track(self):
-        for x in Track.track_list:
-            if x.album == self.name:
+    def add_tracks(self):  # добавить в альбом сразу все треки, относящиеся к этому альбому
+        for x in Track.my_track_list:
+            if self.name == x.album:
                 self.track_list.append(x)
 
     def get_tracks(self):
@@ -36,28 +39,36 @@ class Album:
             x.show()
 
     def get_duration(self):
-        duration = sum((x.duration for x in Track.track_list))
-        print(duration)
-
+        duration = sum((x.duration for x in self.track_list))
+        print(f'The whole duration of the album "{self.name}" is {duration} min')
 
 
 album1 = Album('Shades of Black', 'Kovacs')
 album2 = Album('The game', 'Queen')
 album3 = Album('Группа крови', 'Кино')
 
+track1 = Track('50 Shades of Black', 'Shades of Black', 3)
+track2 = Track('My Love', 'Shades of Black', 5)
+track3 = Track('Whiskey and Fun', 'Shades of Black', 4)
+track4 = Track('Rock it', 'The game', 5)
+track5 = Track('Save me', 'The game', 3)
+track6 = Track('Another one bites the dust', 'The game', 5)
+track7 = Track('Группа крови', 'Группа крови', 5)
+track8 = Track('Война', 'Группа крови', 4)
+track9 = Track('Дальше действовать будем мы', 'Группа крови', 4)
+track10 = Track('Звезда по имени Солнце', 'Группа крови', 4)
 
-
-track1 = Track('50 Shades of Black', 'Kovacs', 3)
-track2 = Track('My Love', 'Kovacs', 5)
-track3 = Track('Whiskey and Fun', 'Kovacs', 4)
-track4 = Track('Rock it', 'Queen', 5)
-track5 = Track('Save me', 'Queen', 3)
-track6 = Track('Another one bites the dust', 'Queen', 5)
-track7 = Track('Группа крови', 'Кино', 5)
-track8 =Track('Война', 'Кино', 4)
-track9 = Track('Дальше действовать будем мы', 'Кино', 4)
-
-
+album1.present()
+album1.add_tracks()
 album1.get_tracks()
+album1.get_duration()
 
+album2.present()
+album2.add_tracks()
+album2.get_tracks()
+album2.get_duration()
 
+album3.present()
+album3.add_tracks()
+album3.get_tracks()
+album3.get_duration()
